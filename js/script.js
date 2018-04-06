469db48b3ca70b374f21ef139e31528b440c9bd6
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-04-06 14:54:57 +0200

    Add script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var btn = document.getElementsByClassName("button");
+var btnQuantity = btn.length;
+console.log(btnQuantity);
+
+
+for (var i = 0 ; i < btnQuantity ; i++) {
+	alert(btn[i].innerText);
+}
+
+	    
+
+
+
